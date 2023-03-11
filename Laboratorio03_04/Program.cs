1f0c56131831eee492ae2756c7f41f4af0ed416f
Ishaimaster <hjflores@correo.url.edu.gt>
2023-03-11 11:44:57 -0600

    Lab3_4

---
+++ Laboratorio03_04/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+//n -> sumatoria = 1 + 2....n
+
+using System.Reflection;
+
+Console.WriteLine("a. Sumatoria");
+Console.WriteLine("b. Tablas de multiplicar");
+
+char opcion = Convert.ToChar(Console.ReadLine());
+
+switch (opcion)
+{
+    case 'a':
+        Console.Write("Ingrese un numero: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+
+        int resultado = 0;
+
+        do
+        {
+            resultado = resultado + n;
+            n--;
+
+        } while (n > 0);
+        Console.WriteLine("Sumatoria=" + resultado);
+
+
+        break;
+    case 'b':
+        for (int i = 1; i < 11; i++)
+        {
+            Console.Write(i + ": ");
+            for (int j = 1; j < 11; j++)
+            {
+                Console.Write((i * j) + "\t");
+            }
+            Console.Write('\n');
+        }
+
+
+        break;
+}
+
+
+
+
+
